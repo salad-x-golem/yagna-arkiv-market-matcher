d19f558407cdb50810c0f88cd1b36d0c4504326e
sieci <sieciech@gmail.com>
2026-01-04 13:50:17 +0100

    Run from loop

--- integration/start_provider.sh
+++ integration/start_provider.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -x
 
-(cd node-deployer/services/upper-0/yagna && ./ya-provider run)&
-(cd node-deployer/services/upper-1/yagna && ./ya-provider run)&
-(cd node-deployer/services/upper-2/yagna && ./ya-provider run)&
-(cd node-deployer/services/upper-3/yagna && ./ya-provider run)&
-(cd node-deployer/services/upper-4/yagna && ./ya-provider run)&
-(cd node-deployer/services/upper-5/yagna && ./ya-provider run)&
-(cd node-deployer/services/upper-6/yagna && ./ya-provider run)&
-(cd node-deployer/services/upper-7/yagna && ./ya-provider run)&
-(cd node-deployer/services/upper-8/yagna && ./ya-provider run)&
-(cd node-deployer/services/upper-9/yagna && ./ya-provider run)&
+for i in {0..9}; do
+  (cd node-deployer/services/upper-"$i"/yagna && ./ya-provider run) &
+done
