b0d4e56a1dc50b4db47325febd0d4cfe7bee54d8
sieci <sieciech@gmail.com>
2026-01-04 13:40:01 +0100

    Try to show rentals

--- integration/start_provider_node.sh
+++ integration/start_provider_node.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 set -x
 
-(cd node-deployer/services/upper-0/yagna && ./yagna-upper-0 service run)
+(cd node-deployer/services/upper-0/yagna && ./yagna-upper-0 service run)&
+(cd node-deployer/services/upper-1/yagna && ./yagna-upper-1 service run)&
+(cd node-deployer/services/upper-2/yagna && ./yagna-upper-2 service run)&
+(cd node-deployer/services/upper-3/yagna && ./yagna-upper-3 service run)&
+(cd node-deployer/services/upper-4/yagna && ./yagna-upper-4 service run)&
+(cd node-deployer/services/upper-5/yagna && ./yagna-upper-5 service run)&
+(cd node-deployer/services/upper-6/yagna && ./yagna-upper-6 service run)&
+(cd node-deployer/services/upper-7/yagna && ./yagna-upper-7 service run)&
+(cd node-deployer/services/upper-8/yagna && ./yagna-upper-8 service run)&
+(cd node-deployer/services/upper-9/yagna && ./yagna-upper-9 service run)&
