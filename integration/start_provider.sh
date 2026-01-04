5db890ae78fb8a644c5bb086d1df99698e64d399
sieci <sieciech@gmail.com>
2026-01-04 22:11:17 +0100

    Silent

--- integration/start_provider.sh
+++ integration/start_provider.sh
@@ -2,5 +2,5 @@
 set -x
 
 for i in $(seq 0 "$1"); do
-  (cd "node-deployer/services/upper-$i/yagna" && ./ya-provider run) &
+  (cd "node-deployer/services/upper-$i/yagna" && ./ya-provider run >/dev/null 2>&1) &
 done
--- integration/start_provider_node.sh
+++ integration/start_provider_node.sh
@@ -2,5 +2,5 @@
 set -x
 
 for i in $(seq 0 "$1"); do
-  (cd "node-deployer/services/upper-$i/yagna" && ./yagna-upper-"$i" service run) &
+  (cd "node-deployer/services/upper-$i/yagna" && ./yagna-upper-"$i" service run >/dev/null 2>&1) &
 done
