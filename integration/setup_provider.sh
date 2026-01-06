4fc8add637ddad2dca77204940bba436c39de37d
scx1332 <sieciech@gmail.com>
2026-01-06 18:13:36 +0100

    Add offer server argument

--- integration/setup_provider.sh
+++ integration/setup_provider.sh
@@ -16,12 +16,13 @@ YAGNA_VERSION=${YAGNA_VERSION}
 MACHINE_PROV=${MACHINE_PROV:-geode}
 MACHINE_PROV_SECRET=${MACHINE_PROV_SECRET:-"abc123"}
 CENTRAL_NET_HOST=${CENTRAL_NET_HOST:-"127.0.0.1:6976"}
+OFFER_SERVER=${OFFER_SERVER:-"http://127.0.0.1:15155"}
 
 git clone git@github.com:salad-x-golem/node-deployer.git
 (cd node-deployer && git clean -fdx && git reset --hard && git checkout provider-only && git pull)
 
 (cd node-deployer && ../venv/bin/python keys.py "${NUMBER_OF_NODES}")
 (cd node-deployer && mkdir "${MACHINE_PROV}_keys" && mv generated_keys/keys.txt "${MACHINE_PROV}_keys/${MACHINE_PROV}.keys" )
-(cd node-deployer && printf "NODE_PREFIX=%s\nNODE_SECRET=%s\nNO_SERVICES=true\nYAGNA_VERSION=${YAGNA_VERSION}\nCENTRAL_NET_HOST=${CENTRAL_NET_HOST}" "${MACHINE_PROV}" "${MACHINE_PROV_SECRET}" > .env )
+(cd node-deployer && printf "NODE_PREFIX=%s\nNODE_SECRET=%s\nNO_SERVICES=true\nYAGNA_VERSION=${YAGNA_VERSION}\nCENTRAL_NET_HOST=${CENTRAL_NET_HOST}\nOFFER_SERVER=${OFFER_SERVER}" "${MACHINE_PROV}" "${MACHINE_PROV_SECRET}" > .env )
 (cd node-deployer && ../venv/bin/python bootstrap.py)
 (cd node-deployer && ./setup-all.sh)
