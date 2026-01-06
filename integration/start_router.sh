9b3cf800cf50ee9d831d9b6203a310c368d74b3b
scx1332 <sieciech@gmail.com>
2026-01-06 15:24:22 +0100

    Fix start router script

--- integration/start_router.sh
+++ integration/start_router.sh
@@ -2,5 +2,5 @@
 set -x
 
 # Start router
-(cd node-deployer/central-net && cp ya-sb-router router-upper)
-(cd node-deployer/central-net && ./router-upper -l tcp://0.0.0.0:6976)
+(cp ya-sb-router router-upper)
+(./router-upper -l tcp://0.0.0.0:6976)
