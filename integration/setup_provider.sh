4565705c2b71f82c70d0bcf68563887823da75d7
scx1332 <sieciech@gmail.com>
2026-01-06 15:10:45 +0100

    Move to env

--- integration/setup_provider.sh
+++ integration/setup_provider.sh
@@ -13,14 +13,15 @@ fi
 
 YAGNA_VERSION=$2
 
-MACHINE_PROV="upper"
-MACHINE_PROV_SECRET="abc123"
+MACHINE_PROV=${MACHINE_PROV:-upper}
+MACHINE_PROV_SECRET=${MACHINE_PROV_SECRET:-"abc123"}
+CENTRAL_NET_HOST=${CENTRAL_NET_HOST:-"127.0.0.1:6976"}
 
 git clone git@github.com:salad-x-golem/node-deployer.git
 (cd node-deployer && git clean -fdx && git reset --hard && git checkout provider-only && git pull)
 
 (cd node-deployer && ../venv/bin/python keys.py "${NUMBER_OF_NODES}")
 (cd node-deployer && mkdir "${MACHINE_PROV}_keys" && mv generated_keys/keys.txt "${MACHINE_PROV}_keys/${MACHINE_PROV}.keys" )
-(cd node-deployer && printf "NODE_PREFIX=%s\nNODE_SECRET=%s\nNO_SERVICES=true\nYAGNA_VERSION=${YAGNA_VERSION}\nCENTRAL_NET_HOST=127.0.0.1:6976" "${MACHINE_PROV}" "${MACHINE_PROV_SECRET}" > .env )
+(cd node-deployer && printf "NODE_PREFIX=%s\nNODE_SECRET=%s\nNO_SERVICES=true\nYAGNA_VERSION=${YAGNA_VERSION}\nCENTRAL_NET_HOST=${CENTRAL_NET_HOST}" "${MACHINE_PROV}" "${MACHINE_PROV_SECRET}" > .env )
 (cd node-deployer && ../venv/bin/python bootstrap.py)
 (cd node-deployer && ./setup-all.sh)
--- integration/setup_requestor.sh
+++ integration/setup_requestor.sh
@@ -2,16 +2,16 @@
 
 YAGNA_VERSION=$1
 
-
-MACHINE_REQ="lower"
-MACHINE_REQ_SECRET="bca321"
+MACHINE_REQ=${MACHINE_REQ:-lower}
+MACHINE_REQ_SECRET=${MACHINE_REQ_SECRET:-bca321}
+CENTRAL_NET_HOSTS=${CENTRAL_NET_HOSTS:-"127.0.0.1:6976"}
 
 git clone git@github.com:salad-x-golem/req-deployer.git
 (cd req-deployer && git clean -fdx && git reset --hard && git checkout main && git pull)
 
 (cd req-deployer && ../venv/bin/python keys.py 1)
 (cd req-deployer && mkdir "${MACHINE_REQ}_keys" && mv generated_keys/keys.txt "${MACHINE_REQ}_keys/${MACHINE_REQ}.keys" )
-(cd req-deployer && printf "NODE_PREFIX=%s\nNODE_SECRET=%s\nNO_SERVICES=true\nYAGNA_VERSION=${YAGNA_VERSION}\nCENTRAL_NET_HOSTS=127.0.0.1:6976" "${MACHINE_REQ}" "${MACHINE_REQ_SECRET}" > .env )
+(cd req-deployer && printf "NODE_PREFIX=%s\nNODE_SECRET=%s\nNO_SERVICES=true\nYAGNA_VERSION=${YAGNA_VERSION}\nCENTRAL_NET_HOSTS=${CENTRAL_NET_HOSTS}" "${MACHINE_REQ}" "${MACHINE_REQ_SECRET}" > .env )
 (cd req-deployer && ../venv/bin/python bootstrap.py)
 
 (cd req-deployer && ./setup-all.sh)
