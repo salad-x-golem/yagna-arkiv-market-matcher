2f42bf828da177661935c854eca2f96729be8e73
scx1332 <sieciech@gmail.com>
2026-01-15 14:45:05 +0100

    Upload env

--- integration/collect_requestor_logs.sh
+++ integration/collect_requestor_logs.sh
@@ -6,7 +6,7 @@ MACHINE_PROV=${MACHINE_PROV:-"geode"}
 
 tar -cf requestor.tar.gz req-deployer/services/lower-0/yagna/.env req-deployer/services/lower-0/yagna/yagnadir
 
-tar -cf vanity-log.tar.gz vanity.log
+tar -cf vanity-log.tar.gz vanity.log req-deployer/services/lower-0/vanity/.env
 mv ../matcher.log .
 tar -cf matcher-log.tar.gz matcher.log
 
